f4b5551963323897497d8030d082357610101ace
slicina7 <slicina1@etf.unsa.ba>
2022-11-17 14:54:33 +0100

    Evaluate method added

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -1,7 +1,30 @@
 package ba.unsa.etf.rpr;
 
+import java.util.Stack;
+
 public class ExpressionEvaluator {
-    Double evaluate(String izraz) {
-        return null;
+    public static Double evaluate(String izraz) {
+        Double broj1,broj2;
+        String znak="";
+        Stack<String> operatori=new Stack<>();
+        Stack<Double> brojevi=new Stack<>();
+        for(String c : izraz.split(" ")){
+            if(c.equals("("));
+            else if(c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/")) operatori.push(c);
+            else if(c.equals(")")) {
+                broj1 = brojevi.peek();
+                brojevi.pop();
+                broj2 = brojevi.peek();
+                brojevi.pop();
+                znak=operatori.peek();
+                if(znak.equals("+")) brojevi.push(broj1+broj2);
+                else if(znak.equals("-")) brojevi.push(broj1-broj2);
+                else if(znak.equals("*")) brojevi.push(broj1*broj2);
+                else if(znak.equals("/")) brojevi.push(broj2/broj1);
+                operatori.pop();
+            }
+            brojevi.push(Double.valueOf(c));
+        }
+        return brojevi.peek();
     }
 }
