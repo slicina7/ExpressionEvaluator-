75f1c330f2dec185ac92bb5e68826f739f7f3cb5
slicina7 <slicina1@etf.unsa.ba>
2022-11-17 17:40:59 +0100

    Changed version

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -4,26 +4,26 @@ import java.util.Stack;
 
 public class ExpressionEvaluator {
     public static Double evaluate(String izraz) {
-        Double broj1,broj2;
-        String znak="";
         Stack<String> operatori=new Stack<>();
         Stack<Double> brojevi=new Stack<>();
         for(String c : izraz.split(" ")){
-            if(c.equals("("));
-            else if(c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/")) operatori.push(c);
-            else if(c.equals(")")) {
-                broj1 = brojevi.peek();
-                brojevi.pop();
-                broj2 = brojevi.peek();
-                brojevi.pop();
-                znak=operatori.peek();
-                if(znak.equals("+")) brojevi.push(broj1+broj2);
-                else if(znak.equals("-")) brojevi.push(broj1-broj2);
-                else if(znak.equals("*")) brojevi.push(broj1*broj2);
-                else if(znak.equals("/")) brojevi.push(broj2/broj1);
-                operatori.pop();
+            if(c.equals("("))
+                continue;
+            else if(c.equals("+") || c.equals("-") || c.equals("*") || c.equals("/") || c.equals("sqrt"))
+            {
+                operatori.push(c);
+                continue;
             }
-            brojevi.push(Double.valueOf(c));
+            else if(c.equals(")")) {
+                Double broj = brojevi.pop();
+                String znak=operatori.pop();
+                if(znak.equals("+")) brojevi.push(brojevi.pop()+broj);
+                else if(znak.equals("-")) brojevi.push(brojevi.pop()-broj);
+                else if(znak.equals("*")) brojevi.push(brojevi.pop()*broj);
+                else if(znak.equals("/")) brojevi.push(brojevi.pop()/broj);
+                else if(znak.equals("sqrt")) brojevi.push(Math.sqrt(broj));
+            }
+            else brojevi.push(Double.parseDouble(c));
         }
         return brojevi.peek();
     }
