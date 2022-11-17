08e36296a9647698c58dcb9da1ba5ff3d495a767
slicina7 <slicina1@etf.unsa.ba>
2022-11-17 22:37:16 +0100

    Added exceptions

--- src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
+++ src/main/java/ba/unsa/etf/rpr/ExpressionEvaluator.java
@@ -3,7 +3,7 @@ package ba.unsa.etf.rpr;
 import java.util.Stack;
 
 public class ExpressionEvaluator {
-    public static Double evaluate(String izraz) {
+    public static Double evaluate(String izraz) throws RuntimeException{
         Stack<String> operatori=new Stack<>();
         Stack<Double> brojevi=new Stack<>();
         for(String c : izraz.split(" ")){
@@ -23,7 +23,13 @@ public class ExpressionEvaluator {
                 else if(znak.equals("/")) brojevi.push(brojevi.pop()/broj);
                 else if(znak.equals("sqrt")) brojevi.push(Math.sqrt(broj));
             }
-            else brojevi.push(Double.parseDouble(c));
+            else {
+                try{
+                    brojevi.push(Double.parseDouble(c));
+                }catch (Exception e){
+                    throw new RuntimeException("Pogresan izraz");
+                }
+            }
         }
         return brojevi.peek();
     }
